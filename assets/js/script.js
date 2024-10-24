0cf6f74e45ce8e1e8260279519f4ad2dd0b7a217
Andy Tran <rooneytai5236@gmail.com>
2024-10-24 18:57:52 +1100

    Update UI

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["backend engineer", "frontend developer", "software engineer", "full-stack engineer"],
+    strings: ["Backend Engineer", "Frontend Developer", "Software Engineer", "Full-Stack Engineer"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
